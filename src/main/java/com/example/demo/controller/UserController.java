7be5d9cce389c7b93b6191ee0708ac7bc40da8c2
zhubin <1342018225@qq.com>
2020-10-20 17:55:52 +0800

    10.20 17.55

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhubin
+ * @since 2020-10-20 17:49
+ */
+@RestController
+@RequestMapping("user")
+public class UserController {
+}
